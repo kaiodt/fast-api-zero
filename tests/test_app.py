65eb0fa48f51eab7cb71ef9e0fd6912759549828
Kaio Rocha <kaiodtr@gmail.com>
2024-03-30 20:38:43 -0300

    dev: atualização dos endpoints e testes para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_api_zero.schemas import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -23,47 +26,56 @@ def test_create_user(client):
     }
 
 
+def test_create_user_returns_400_when_user_already_exists(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': user.username,
+            'email': user.email,
+            'password': user.password,
+        },
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username already registered'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
-    response = client.get('/users/1')
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+
+    assert response.status_code == 200
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user(client, user):
+    response = client.get(f'/users/{user.id}')
 
     assert response.status_code == 200
     assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
-        'id': 1,
+        'username': user.username,
+        'email': user.email,
+        'id': user.id,
     }
 
 
 def test_read_user_returns_404_when_not_found(client):
-    response_negative_id = client.get('/users/-1')
+    response = client.get('/users/-1')
 
-    response_out_of_bound_id = client.get('/users/1000000000')
-
-    assert response_negative_id.status_code == 404
-    assert response_negative_id.json() == {'detail': 'User not found'}
-
-    assert response_out_of_bound_id.status_code == 404
-    assert response_out_of_bound_id.json() == {'detail': 'User not found'}
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -80,7 +92,7 @@ def test_update_user(client):
 
 
 def test_update_user_returns_404_when_not_found(client):
-    response_negative_id = client.put(
+    response = client.put(
         '/users/-1',
         json={
             'username': 'bob',
@@ -89,36 +101,19 @@ def test_update_user_returns_404_when_not_found(client):
         },
     )
 
-    response_out_of_bound_id = client.put(
-        '/users/1000000000',
-        json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
-        },
-    )
-
-    assert response_negative_id.status_code == 404
-    assert response_negative_id.json() == {'detail': 'User not found'}
-
-    assert response_out_of_bound_id.status_code == 404
-    assert response_out_of_bound_id.json() == {'detail': 'User not found'}
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
-    response = client.delete('/users/1')
+def test_delete_user(client, user):
+    response = client.delete(f'/users/{user.id}')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
 
 
 def test_delete_user_returns_404_when_not_found(client):
-    response_negative_id = client.delete('/users/-1')
+    response = client.delete('/users/-1')
 
-    response_out_of_bound_id = client.delete('/users/1000000000')
-
-    assert response_negative_id.status_code == 404
-    assert response_negative_id.json() == {'detail': 'User not found'}
-
-    assert response_out_of_bound_id.status_code == 404
-    assert response_out_of_bound_id.json() == {'detail': 'User not found'}
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
